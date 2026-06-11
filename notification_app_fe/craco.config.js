a6a73904a33f2c3f34d49985ed405fa897fd258b
Yuvraj Singh <mr.yuvrajsinghh@gmail.com>
2026-06-11 14:22:43 +0530

    Downgrade MUI to v5 for CRA compatibility, update CRACO config

--- notification_app_fe/craco.config.js
+++ notification_app_fe/craco.config.js
@@ -1,11 +1,21 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // Fix 1: treat .mjs files as regular JS (not strict ESM)
       webpackConfig.module.rules.push({
         test: /\.mjs$/,
         include: /node_modules/,
         type: 'javascript/auto',
+        resolve: { fullySpecified: false },
       });
+
+      // Fix 2: disable fullySpecified for all .m?js imports from node_modules
+      webpackConfig.module.rules.push({
+        test: /\.m?js/,
+        include: /node_modules/,
+        resolve: { fullySpecified: false },
+      });
+
       return webpackConfig;
     },
   },
